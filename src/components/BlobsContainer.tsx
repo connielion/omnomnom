f94032cdaa638c4b5d448e8d4251bfeb4a9223dd
Paul Gonzales <paulgonzales@MBA.local>
2021-08-26 11:35:25 -0700

    feat(blob-container):style the blob grid container

--- src/components/BlobsContainer.tsx
+++ src/components/BlobsContainer.tsx
@@ -2,27 +2,52 @@ import { render } from "@testing-library/react";
 import React from "react";
 import styled from 'styled-components';
 import { topIngredientsList } from "../util/topIngredientsList";
+import ingredientBlob from '../assets/blob.svg';
 
 const BlobsContainer = () => {
     const BlobsContainer = styled.div`
         grid-area: 1 / 2 / 2 / 3;
-        background-color: antiquewhite;
+        background-color: #ebe6da;
         padding: 10px;
 
         display: grid;
         grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-auto-rows: 250px;
         grid-gap: 10px;
     `; 
 
     const Blob = styled.div`
-        background-color: red;
+        background-image: url(${ingredientBlob});
+        background-size: 120%;
+        background-repeat: no-repeat;
+        background-position: center;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
     `;
+
+    const IngredientName = styled.h1`
+        font-size: 24px;
+        color: #fff;
+    `;
+
+    const H1Container = styled.div`
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `
 
     const renderIngredientBlob = () => {
         const renderBlob = topIngredientsList.map((ingredient) => {
             return (
                 <Blob>
-                    <h1>{ingredient.name}</h1>
+                    <H1Container>
+                        <IngredientName>
+                            {ingredient.name}
+                        </IngredientName>
+                    </H1Container>
                 </Blob>)
         })
 
